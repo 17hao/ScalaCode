ac13275b076cba76b12c39ebee6d3ebbbc71cd71
sqh <812411866@qq.com>
2019-01-27 12:44:12 +0800

    update

---
+++ helloscala/src/functional/PartialFunc.scala
@@ -0,0 +1,25 @@
+package functional
+
+object PartialFunc {
+  def main(args: Array[String]): Unit = {
+    val handle1to6 = convert1to3 orElse convert4to6
+    println(handle1to6(1))
+    println(handle1to6(5))
+  }
+
+  val convert1to3: PartialFunction[Int, String] = new PartialFunction[Int, String] {
+    val nums = Array("one", "two", "three")
+
+    def apply(i: Int): String = nums(i - 1)
+
+    override def isDefinedAt(x: Int): Boolean = x > 0 && x < 3
+  }
+
+  val convert4to6: PartialFunction[Int, String] = new PartialFunction[Int, String] {
+    val nums = Array("four", "five", "six")
+
+    def apply(i: Int): String = nums(i - 4)
+
+    override def isDefinedAt(x: Int): Boolean = x > 3 && x < 7
+  }
+}
--- helloscala/src/transfer/VarTransfer.scala
+++ helloscala/src/transfer/VarTransfer.scala
@@ -3,12 +3,17 @@ package transfer
 object VarTransfer {
   def main(args: Array[String]): Unit = {
     val num = 1
-    val num2: Double = num
-    //val num3:Byte = num // num:Int不能转换成Byte类型
     println(num.toDouble)
     println(num.toLong)
     println(num.toByte)
+
+    val num2: Double = num
     println(num2)
+
+    //val num3:Byte = num // num:Int不能转换成Byte类型
     //print(num3)
+
+    val num4 = 3.5
+    println(num4.toInt)
   }
 }

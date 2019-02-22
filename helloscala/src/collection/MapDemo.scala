c6979b66280c1e05298ceb5ebb09063c60f35864
17hao <812411866@qq.com>
2019-02-22 17:23:11 +0800

    标识符末尾不应带'_'

--- helloscala/src/collection/MapDemo.scala
+++ helloscala/src/collection/MapDemo.scala
@@ -8,10 +8,10 @@ object MapDemo {
     println(map)
     // map(1) = "one_" 会出错，因为immutable.Map本身是不可变的
 
-    var map_ = scala.collection.mutable.Map[String, String]()
-    map_ += ("1" -> "one")
-    println(map_)
-    map_("1") = "one_"
-    println(map_)
+    var map2 = scala.collection.mutable.Map[String, String]()
+    map2 += ("1" -> "one")
+    println(map2)
+    map2("1") = "one_"
+    println(map2)
   }
 }
--- helloscala/src/functional/CurryingDemo.scala
+++ helloscala/src/functional/CurryingDemo.scala
@@ -6,8 +6,8 @@ object CurryingDemo {
     // 多个参数列表，调用函数时使用了较少的参数时，自动将缺失的作为参数
     val res = numbers.foldLeft(0)((m, n) => m + n)
     // 使用类型推断，使代码更简洁
-    val res_ = numbers.foldLeft(1)(_ * _)
+    val resAnother = numbers.foldLeft(1)(_ * _)
     println(res)
-    println(res_)
+    println(resAnother)
   }
 }

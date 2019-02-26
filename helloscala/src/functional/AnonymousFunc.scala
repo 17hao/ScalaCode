9ad1ca970be7ff19c04ef9987d60adc8f58be1c6
17hao <812411866@qq.com>
2019-02-26 13:32:43 +0800

    update

--- helloscala/src/functional/AnonymousFunc.scala
+++ helloscala/src/functional/AnonymousFunc.scala
@@ -1,15 +1,15 @@
 package functional
 
-// 函数可以作为变量传递
 object AnonymousFunc {
+  // 函数(方法）可以作为变量传递
   def main(args: Array[String]): Unit = {
-    oncePerSecond(() => println("anonymous->time flies like an arrow..."))
+    oncePerSecond(println("anonymous->time flies like an arrow..."))
     oncePerSecond(timeFlies)
   }
 
-  def oncePerSecond(callback: () => Unit): Unit = {
-    for (i <- 1 to 3) {
-      callback()
+  def oncePerSecond(callback: => Unit): Unit = {
+    for (_ <- 1 to 3) {
+      callback
     }
   }
 
---
+++ helloscala/src/functional/FuncAsArg.scala
@@ -0,0 +1,23 @@
+package functional
+
+object FuncAsArg {
+  // 接受函数作(方法）为参数
+  def main(args: Array[String]): Unit = {
+    execFunction(printName)
+    println(hello("hello"))
+  }
+
+  // 并不是纯粹的函数式风格
+  def execFunction(f: String => Unit): Unit = {
+    f("sqh")
+  }
+
+  def printName(name: String): Unit = {
+    println("hello: " + name)
+  }
+
+  // 接受函数（方法）作为参数意味着（基本上）函数是匿名的
+  def hello(f: => String) = {
+    f + "sqh"
+  }
+}

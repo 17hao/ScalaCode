9d8b3f2299172312ccac3c6a642f10b938dda3fe
17hao <812411866@qq.com>
2019-03-01 15:27:53 +0800

    tuple、set、map的使用

---
+++ ProgrammingInScala/src/charpter3/UseOfMap.scala
@@ -0,0 +1,12 @@
+package charpter3
+
+object UseOfMap {
+  def main(args: Array[String]): Unit = {
+    import scala.collection.mutable
+    // 在任意Scala对象上调用'->'方法会返回一个包含左右2个元素的元组,实际上是隐式转换的机制
+    println(1 -> "sqh")
+    val nameMap = mutable.Map[Int, String]()
+    nameMap += (1 -> "sqh")
+    println(nameMap)
+  }
+}
---
+++ ProgrammingInScala/src/charpter3/UseOfTuple.scala
@@ -0,0 +1,14 @@
+package charpter3
+
+object UseOfTuple {
+  def main(args: Array[String]): Unit = {
+    val tuple = (1, "two", 3.0)
+    println(tuple)
+    /*
+      获取元组中的元素用'._x',x从1开始
+      因为元组可以存放不同类型的数据
+      来自其他语言的传统，如Haskell、ML
+    */
+    println(tuple._1)
+  }
+}

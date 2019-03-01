5c66a6511894a57ee08f352bf0285b92fb75ed83
17hao <812411866@qq.com>
2019-03-01 21:24:11 +0800

    函数式对象Rational

---
+++ ProgrammingInScala/src/chapter6/Demo.scala
@@ -0,0 +1,11 @@
+package chapter6
+
+object Demo {
+  def main(args: Array[String]): Unit = {
+    val num1 = new Rational(1, 4)
+    val num2 = new Rational(1, 4)
+    val num3 = new Rational(1, 4)
+    val sum = num1 + num2 + num3
+    println(sum)
+  }
+}

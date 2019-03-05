90912517b406ea6de05881c843cdde69e9033223
17hao <812411866@qq.com>
2019-03-05 23:43:52 +0800

    Element

--- ProgrammingInScala/src/chapter10/ElementTest.scala
+++ ProgrammingInScala/src/chapter10/ElementTest.scala
@@ -4,4 +4,8 @@ object ElementTest extends App {
   val arrayElement = new ArrayElement(Array("***hello", "hello***"))
   println(arrayElement.width)
   println(arrayElement)
+  val lineElement = new LineElement("sqh")
+  println(lineElement)
+  val uniformElement = new UniformElement('a', 2, 3)
+  println(uniformElement)
 }
---
+++ ProgrammingInScala/src/chapter10/UniformElement.scala
@@ -0,0 +1,14 @@
+package chapter10
+
+/*
+  指定长和宽，由单一字符组成的二维图案
+ */
+class UniformElement(
+                      ch: Char,
+                      override val width: Int,
+                      override val height: Int
+                    ) extends Element {
+  private val line = ch.toString * width
+
+  override def contents: Array[String] = Array.fill(height)(line)
+}

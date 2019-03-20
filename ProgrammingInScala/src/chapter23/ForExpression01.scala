ab81ebb5c364175c6df659de4c2778a7360cd038
17hao <812411866@qq.com>
2019-03-20 21:17:33 +0800

    for expression

---
+++ ProgrammingInScala/src/chapter23/ForExpression01.scala
@@ -0,0 +1,29 @@
+package chapter23
+
+object ForExpression01 extends App {
+  val lara = Person("lara", false)
+  val bob = Person("bob", true)
+  val julie = Person("julie", false, lara, bob)
+  val persons = List(lara, bob, julie)
+  //val res = persons.filter(!_.isMale).map(p => p.children.map(c => (p.name, c.name)))
+  val flatMapRes = persons.filter(!_.isMale).flatMap(p => p.children.map(c => (p.name, c.name)))
+  //println(middle)
+  println(flatMapRes)
+
+  val forRes = for {
+    p <- persons
+    if (!p.isMale)
+    c <- p.children
+  } yield (p.name, c.name)
+  println(forRes)
+
+  //val list = List(null, "1", "2")
+  //val tmp = list.map(l => (l,"3"))
+  //println(tmp)
+}
+
+case class Person(
+                   name: String,
+                   isMale: Boolean,
+                   children: Person*
+                 )

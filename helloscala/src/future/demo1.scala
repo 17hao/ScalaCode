f0655bffd31d81a1c5a681557a27f2d6d5cf09c1
17hao <sqh1107@gmail.com>
2019-05-11 09:47:01 +0800

    update

--- helloscala/src/future/demo1.scala
+++ helloscala/src/future/demo1.scala
@@ -2,23 +2,26 @@ package future
 
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
 
-object demo1 extends App {
+/**
+ * @author Shi Qihao
+ * @date 2019-05-10 19:20
+ */
+object Demo1 extends App {
   implicit val ec: ExecutionContextExecutor = ExecutionContext.global
 
   def addTwo(n: Int): Future[Int] = Future {
     n + 2
   }
 
-  def addTwoAndDouble(n: Int): Future[Int] = addTwo(n).map(_ * 2)
+  def addTwoAndDouble(n: Int): Future[Int] = addTwo(n).map(i => i * 2)
 
-  def addTwoAndAddTwo(n: Int) = addTwo(n).map(int => addTwo(int))
+  def addTwoAndAddTwo(n: Int) = addTwo(n).map(i => addTwo(i))
 
   val n = 2
-
-  println(addTwo(n)) //为什么没有这个表达式就不出结果
+  println(addTwo(n))
   addTwo(n).onComplete(println(_))
-
+  println(addTwoAndDouble(n))
   addTwoAndDouble(n).onComplete(println(_))
-
+  println(addTwoAndAddTwo(n))
   addTwoAndAddTwo(n).onComplete(println(_))
 }

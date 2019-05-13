89dbade504b4ad78556025a7e69f86b654b2b102
17hao <sqh1107@gmail.com>
2019-05-13 11:58:19 +0800

    Future

--- helloscala/src/future/demo1.scala
+++ helloscala/src/future/demo1.scala
@@ -1,22 +1,30 @@
 package future
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import scala.util.{Failure, Success}
 
 object demo1 extends App {
-  implicit val ec = ExecutionContext.global
+  implicit val ec: ExecutionContextExecutor = ExecutionContext.global
 
-  def addTwo(n: Int): Future[Int] = {
-    Future(n + 2)
-  }
+  def addTwo(n: Int): Future[Int] = Future(n + 2)
 
   def addTwoAndDouble(n: Int) = addTwo(n).map(_ * 2)
 
-  def addTwoAndAddTwo(n: Int) = addTwo(n).map(addTwo(_))
+  def addTwoAndAddTwo(n: Int) = addTwo(n).flatMap(addTwo)
 
   val n = 2
-  addTwo(n).onComplete(println(_))
+  addTwo(n).onComplete {
+    case Success(result) => println(result)
+    case Failure(exception) => println(exception)
+  }
 
-  addTwoAndDouble(n).onComplete(println(_))
+  addTwoAndDouble(n).onComplete {
+    case Success(result) => println(result)
+    case Failure(exception) => println(exception)
+  }
 
-  addTwoAndAddTwo(n).onComplete(println(_))
+  addTwoAndAddTwo(n).onComplete {
+    case Success(result) => println(result)
+    case Failure(exception) => println(exception.toString)
+  }
 }

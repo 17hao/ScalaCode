546015a69379114714b6c53163e668028615ac97
17hao <sqh1107@gmail.com>
2019-05-30 17:52:21 +0800

    pass by name or value

---
+++ helloscala/src/fp/PassByNameOrValue.scala
@@ -0,0 +1,32 @@
+package fp
+
+object PassByNameOrValue {
+  def doSomething(): Int = {
+    println("do something")
+    1
+  }
+
+  /**
+    *
+    * @param x just an Integer
+    */
+  def passByValue(x: Int): Unit = {
+    println("the value is: " + x)
+    println("the value is: " + x)
+  }
+
+  /**
+    *
+    * @param x a function of no argument that will generate an Integer
+    */
+  def passByName(x: => Int): Unit = {
+    println("the value is: " + x)
+    println("the value is: " + x)
+  }
+
+  def main(args: Array[String]): Unit = {
+    passByValue(doSomething())
+    println("##########")
+    passByName(doSomething())
+  }
+}

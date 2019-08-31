3c00205ab18f1ccef7b3f7f4b786743f9c560d07
17hao <sqh1107@gmail.com>
2019-08-31 11:36:12 +0800

    update json demo

--- src/helloscala/JsonDemo.scala
+++ src/helloscala/JsonDemo.scala
@@ -8,17 +8,23 @@ object JsonDemo extends App {
 
   val s = Seq("1", "2", "3")
 
-  val k = List("k1", "k2")
+  val params = List("{k1}", "{k2}")
 
-  val v = List("v1", "v2")
+  val values = Seq("v1", "v2")
 
-  val m = k.zip(v)
+  val m = params.map(_.replaceAll("[{}]", "")).zip(values)
+
+  val ss: Seq[Seq[String]] = Seq(Seq("p1", "n1"), Seq("p2", "n2"))
+
+  val b = ss.map(s => params.map(_.replaceAll("[{}]", "")).zip(s).toMap)
 
   println(new Gson().toJson(new BagOfPrimitives))
   println(new Gson().toJson(s.asJava))
   println(m.toMap)
   println(new Gson().toJson(m.toMap.asJava))
-
+  println(b.asJava)
+  println(new Gson().toJson(b.map(m => m.asJava).asJava))
+  println(new Gson().toJson(b.asJava))
   val map = Map[String, String](
     "k1" -> "v1",
     "k2" -> "v2"

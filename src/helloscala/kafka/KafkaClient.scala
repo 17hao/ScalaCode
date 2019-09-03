4ad60f95258041cfaa8f8da4ad4ddfc2a9b12644
17hao <sqh1107@gmail.com>
2019-09-03 17:21:37 +0800

    kafka consumer

--- src/helloscala/kafka/KafkaClient.scala
+++ src/helloscala/kafka/KafkaClient.scala
@@ -1,7 +1,9 @@
 package helloscala.kafka
 
+import java.util
 import java.util.Properties
 
+import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 object KafkaClient extends App {
@@ -13,4 +15,15 @@ object KafkaClient extends App {
   val record = new ProducerRecord[String, String]("test", "produce a message")
   producer.send(record)
   producer.close()
+
+  properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  properties.put("group.id", "test-consumer-group")
+  val consumer = new KafkaConsumer[String, String](properties)
+  consumer.subscribe(util.Arrays.asList("test"))
+  while (true) {
+    val records = consumer.poll(100)
+    records.forEach(record => println(s"key is: ${record.key}; value is: ${record.value()}"))
+  }
+  consumer.close()
 }

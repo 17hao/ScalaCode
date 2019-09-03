b7b784c7f860416268f9bcfb0c98529e37f4adfb
17hao <sqh1107@gmail.com>
2019-09-03 21:01:19 +0800

    protobuf

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,8 @@ object Dependencies {
   val gson = "com.google.code.gson" % "gson" % "2.6.2"
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.5.23"
   val kafka = "org.apache.kafka" % "kafka-clients" % "1.0.1"
+  val protobuf = "com.google.protobuf" % "protobuf-java" % "3.8.0"
 
   // Projects
-  val dependencies: Seq[ModuleID] = Seq(rediscala, gson, akkaActor, kafka)
+  val dependencies: Seq[ModuleID] = Seq(rediscala, gson, akkaActor, kafka, protobuf)
 }

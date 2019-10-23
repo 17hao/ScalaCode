0d8191e699eaf21a8e554a378087ce3ec505ecb5
17hao <sqh1107@gmail.com>
2019-10-23 10:57:41 +0800

    guava cache demo

---
+++ src/main/scala/xyz/shiqihao/guava/CacheDemo.scala
@@ -0,0 +1,14 @@
+package xyz.shiqihao.guava
+
+import java.util.concurrent.TimeUnit
+
+import com.google.common.cache.{Cache, CacheBuilder}
+
+object CacheDemo extends App {
+  val cache: Cache[String, String] = CacheBuilder.newBuilder()
+    .maximumSize(3)
+    .expireAfterWrite(10, TimeUnit.SECONDS)
+    .build()
+  cache.put("k1", "v1")
+  println(cache.getIfPresent("k1"))
+}

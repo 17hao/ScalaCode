41518b872a2d1babaf65c3991cb18c5d18ea77da
17hao <sqh1107@gmail.com>
2019-11-07 18:56:12 +0800

    future

---
+++ src/main/scala/xyz/shiqihao/misc/future/FutureCombinatorsTest.scala
@@ -0,0 +1,29 @@
+package xyz.shiqihao.misc.future
+
+import org.scalatest.{FunSpec, Matchers}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class FutureCombinatorsTest extends FunSpec with Matchers with ConcurrentUtils {
+
+  describe("Futures") {
+    it("could be composed using map") {
+      val futureCombinators = new FutureCombinators()
+      //val result = timed(Await.result(futureCombinators.sumOfThreeNumbersSequentialMap(), 7 seconds))
+      //result shouldBe 6
+      val result = timed(Await.result(futureCombinators.sumOfThreeNumbersParallelMapForComprehension(), 7 seconds))
+      result shouldBe 6
+    }
+  }
+}
+
+trait ConcurrentUtils {
+  def timed[T](block: => T): T = {
+    val start = System.currentTimeMillis()
+    val result = block
+    val duration = System.currentTimeMillis() - start
+    println(s"Time taken : $duration")
+    result
+  }
+}
